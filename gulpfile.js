24a418106be4f2f2a07ffcc4522189940a54f17b
Rafael Grillo <grillorafael@gmail.com>
2016-02-20 13:11:37 +0000

    Improve gulp scripts

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@
 
     const gulp = require('gulp');
     const babel = require('gulp-babel');
-    const  sourcemaps = require('gulp-sourcemaps');
+    const sourcemaps = require('gulp-sourcemaps');
+    const webserver = require('gulp-webserver');
+    const sass = require('gulp-sass');
 
-    gulp.task('build', () => {
+    gulp.task('js:build', () => {
         return gulp.src('js/timeline-chart.js')
             .pipe(sourcemaps.init())
             .pipe(babel())
@@ -13,7 +15,29 @@
             .pipe(gulp.dest('dist'));
     });
 
-    gulp.task('default', () => {
-        return gulp.watch('js/*', ['build']);
+    gulp.task('js:watch', () => {
+        return gulp.watch('js/*', ['js:build']);
     });
+
+    gulp.task('css:build', function () {
+      return gulp.src('./scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist'));
+    });
+
+    gulp.task('css:watch', () => {
+        return gulp.watch('scss/*', ['css:build']);
+    });
+
+    gulp.task('serve', () => {
+        gulp.src('./')
+            .pipe(webserver({
+                livereload: true,
+                directoryListing: true,
+                open: true
+            }));
+    });
+
+
+    gulp.task('default', ['js:watch', 'css:watch', 'serve']);
 })();
